808f5134fe0a0c5b8afa17a02a9f88f8fa38ccb7
Thiago Coelho <coelh@10.50.16.231>
2018-05-02 14:09:35 -0400

    Terminando o projeto/ conectando com o banco de dados

--- Tarefa02-05/src/br/edu/ifro/MenuPrincipalController.java
+++ Tarefa02-05/src/br/edu/ifro/MenuPrincipalController.java
@@ -5,14 +5,18 @@
  */
 package br.edu.ifro;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
@@ -24,6 +28,8 @@ public class MenuPrincipalController implements Initializable {
     private TextField txtNum1,txtNum2,txtResult;
     @FXML
     private Button btnSoma;
+    @FXML
+    private Label label;
     
     @FXML
     private void Soma(ActionEvent event) {
@@ -37,5 +43,25 @@ public class MenuPrincipalController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void AbrirCadastroAluno(ActionEvent event) {
+   try{
+       FXMLLoader fxmlLoader = new FXMLLoader();
+    fxmlLoader.setLocation(getClass().getResource("Aluno.fxml"));
+       Scene scene = new Scene(fxmlLoader.load(),900,682);
+       Stage stage = new Stage();
+        stage.setTitle("Cadastrar aluno");
+        stage.setScene(scene);
+        stage.show();
+}
+catch(IOException e){
+
+}
+    }
+
+    @FXML
+    private void FecharProjeto(ActionEvent event) {
+    }
     
 }
